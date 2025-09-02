406e4e283cde497ab09b89ccf3b7196d953d9c38
AmMurrr <gribvsevid@gmail.com>
2025-09-02 18:41:16 +0300

    added static root

--- random_quote_project/settings.py
+++ random_quote_project/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'random_quote_project/static'
+
 # Для медиафайлов 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'random_quote_project/media'
